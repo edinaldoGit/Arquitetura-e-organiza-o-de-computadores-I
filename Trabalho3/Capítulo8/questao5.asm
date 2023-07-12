8a72c9ef8460bafbd9c730657069192bd6654cb6
joaomarceloalencar <joao.marcelo@ufc.br>
2023-07-12 14:41:53 -0300

    Correção do Capítulo 08

--- Trabalho3/Capítulo8/questao5.asm
+++ Trabalho3/Capítulo8/questao5.asm
@@ -1,3 +1,4 @@
+; Correção: 1,0
 ;   Building Instructions:
 ;   ml /Cx /coff questao5.asm /link /SUBSYSTEM:console /out:questao5.exe kernel32.lib legacy_stdio_definitions.lib msvcrt.lib
             .686

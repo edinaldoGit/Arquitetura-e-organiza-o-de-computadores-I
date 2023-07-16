6dd6c0486cd9dd1c2ff51598181c06266d20f942
joaomarceloalencar <joao.marcelo@ufc.br>
2023-07-16 15:38:20 -0300

    Correção da Questão 03 do Capítulo 10.

--- Trabalho3/Capítulo10/questao4.asm
+++ Trabalho3/Capítulo10/questao4.asm
@@ -1,3 +1,4 @@
+; Correção: 0,5
 ;   Building instructions:
 ;   ml /Cx /coff questao4.asm /link /SUBSYSTEM:console /out:questao4.exe kernel32.lib legacy_stdio_definitions.lib msvcrt.lib
             .686
